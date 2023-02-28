be59763350665b9ecdd928551800b5e92718a039
Ricardo Meneses Morales <ricardomeneses0503@gmail.com>
2023-02-28 16:19:22 -0600

    Finished project

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import Formulario from './components/Formulario';
+import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
+
+function App() {
+  const [monenedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(monenedas).length) {
+      const cotizarCripto = async () => {
+        setCargando(true);
+        setResultado({});
+        const { moneda, criptoMoneda } = monenedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado(resultado.DISPLAY[criptoMoneda][moneda]);
+        setCargando(false);
+      };
+      cotizarCripto();
+    }
+  }, [monenedas]);
+  return (
+    <Contenedor>
+      <Imagen src='/img/imagen-criptos.png' alt='Criptos' />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario setMonedas={setMonedas} />
+
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado} />}
+      </div>
+    </Contenedor>
+  );
+}
+
+export default App;
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
